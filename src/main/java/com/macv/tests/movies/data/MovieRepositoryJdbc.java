ce1a1113a608105cba6fe610843967905a2a1bc5
Mateo Vasco <macv9620@utp.edu.co>
2023-12-02 05:19:46 -0500

    Se crea test de integración

---
+++ src/main/java/com/macv/tests/movies/data/MovieRepositoryJdbc.java
@@ -0,0 +1,43 @@
+package com.macv.tests.movies.data;
+
+import com.macv.tests.movies.model.Genre;
+import com.macv.tests.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class MovieRepositoryJdbc implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MovieRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+
+        return jdbcTemplate.query("SELECT * FROM movies", movieMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+    private static final RowMapper<Movie> movieMapper = (rs, rowNum) -> new Movie(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getInt("minutes"),
+            Genre.valueOf(rs.getString("genre"))
+    );
+}
